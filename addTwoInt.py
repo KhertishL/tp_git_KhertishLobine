0af56a941d3f8a6b48956351224b8060e48e7fd9
KhertishL <109499710+KhertishL@users.noreply.github.com>
2022-07-20 18:23:12 +0400

    Update addTwoInt.py

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,12 +1,22 @@
-#!/usr/env python
+#!/bin/env python
 
 import sys
 
 def add(a,b):
 	sum = a + b
 	print (sum)
+try:
+	a = int(sys.argv[1])
+	b = int(sys.argv[2])
+except IndexError:
+	print("Incorrect number of arguments!")
+	sys.exit(0)
 
-a = int(sys.argv[1])
-b = int(sys.argv[2])
+
+if len(sys.argv) != 3:
+		
+	print("Incorrect number of arguments!")
+	sys.exit(0)
 
 add(a,b)
+
